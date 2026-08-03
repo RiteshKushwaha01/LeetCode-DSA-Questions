b4c5c3c444ad71325de8ee466f9a8d2bc70d1aa7
Ritesh Kushwaha <riteshkushwaha089@gmail.com>
2026-08-03 21:59:12 +0530

    Time: 29 ms (48.5%), Space: 25.4 MB (7.52%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int maxDistinct(const std::string& s) {
-    unordered_set<char> seen;
-    int count = 0;
-    
-    for (size_t i = 0; i < s.length(); i++) {
-        if (seen.count(s[i]) == 0) { 
-            count++;
-            seen.insert(s[i]);
+    int maxDistinct(string s) {
+        unordered_set<char> seen;
+        int count = 0;
+
+        for (size_t i = 0; i < s.length(); i++) {
+            if (seen.count(s[i]) == 0) {
+                count++;
+                seen.insert(s[i]);
+            }
         }
+        return count;
     }
-    return count;
-}
 };
