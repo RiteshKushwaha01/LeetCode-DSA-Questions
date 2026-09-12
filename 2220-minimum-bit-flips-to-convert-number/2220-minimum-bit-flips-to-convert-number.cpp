bd39947dd5dfa0f2e3c9f5c85c1a968db08d0663
Ritesh Kushwaha <riteshkushwaha089@gmail.com>
2026-09-12 18:47:31 +0530

    Time: 0 ms (100%), Space: 8.1 MB (26.37%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorSum = start ^ goal;
+        int cnt = 0;
+        for (int i = 0; i < 31; i++) {
+            if (xorSum & (1 << i))
+                cnt++;
+        }
+        return cnt;
+    }
+};
